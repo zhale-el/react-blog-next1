2115d8dfd7343201fb83fa40744206841c6c559a
zhaleh <zhalehelmi@gmail.com>
2024-02-18 16:07:43 +0330

    add header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Header from "./components/Header/Header";
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
 }
 
 export default App;
